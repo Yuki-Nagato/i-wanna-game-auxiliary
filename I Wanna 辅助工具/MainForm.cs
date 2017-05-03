61312b3e55b81fcaf235e2bf5b597851f426b199
Yuki-Nagato <su_zhihua_2007@163.com>
2017-05-04 00:05:53 +0800

    Revert "Revert "first commit""
    This reverts commit a751c66e4db50bbf44e3d2275ce7d1fdd7df57cf.

---
+++ I Wanna 辅助工具/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace I_Wanna_辅助工具
+{
+    public partial class MainForm : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern IntPtr GetForegroundWindow();
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int GetWindowText(IntPtr hwnd, StringBuilder IpString, int cch);
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKeepFire_Click(object sender, EventArgs e)
+        {
+            KeepFire fm = new KeepFire();
+            fm.Show();
+        }
+        public static bool IsIWannaActived()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            StringBuilder Head=new StringBuilder("                ");
+            GetWindowText(hwnd, Head, 16);
+            string fh=Head.ToString();
+            if (fh.StartsWith("i wanna")||fh.StartsWith("     ")||fh.StartsWith("I wanna"))
+            {
+                return true;
+            }
+                else
+            {
+                return false;
+            }
+        }
+
+        private void btnSmallJump_Click(object sender, EventArgs e)
+        {
+            SmallJump sj = new SmallJump();
+            sj.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JumpOppositeStab fm = new JumpOppositeStab();
+            fm.Show();
+        }
+
+        private void btnKeepJump_Click(object sender, EventArgs e)
+        {
+            KeepJump kj = new KeepJump();
+            kj.Show();
+        }
+    }
+}
